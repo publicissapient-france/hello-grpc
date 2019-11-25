dd74b5004b4861bcc347ff7cdae79c7d8fcc2622
Qian Jin <qian.jin.dev@gmail.com>
2019-11-25 15:46:00 +0100

    use kotlin coroutines for android example

--- app/src/main/java/fr/xebia/hellogrpc/MainActivity.kt
+++ app/src/main/java/fr/xebia/hellogrpc/MainActivity.kt
@@ -1,56 +1,68 @@
 package fr.xebia.hellogrpc
 
-import android.app.Activity
 import android.content.Context
-import android.os.AsyncTask
 import android.os.Bundle
-import android.text.TextUtils
 import android.text.method.ScrollingMovementMethod
-import android.view.View
 import android.view.inputmethod.InputMethodManager
-import android.widget.Button
-import android.widget.TextView
+import androidx.annotation.WorkerThread
 import androidx.appcompat.app.AppCompatActivity
 import io.grpc.ManagedChannel
 import io.grpc.ManagedChannelBuilder
 import kotlinx.android.synthetic.main.activity_main.*
+import kotlinx.coroutines.Dispatchers
+import kotlinx.coroutines.GlobalScope
+import kotlinx.coroutines.launch
+import kotlinx.coroutines.withContext
 import java.io.PrintWriter
 import java.io.StringWriter
-import java.lang.ref.WeakReference
 import java.util.concurrent.TimeUnit
 
-class MainActivity : AppCompatActivity(), View.OnClickListener {
+class MainActivity : AppCompatActivity() {
+
+    private var channel: ManagedChannel? = null
+
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_main)
-        grpc_response_text!!.movementMethod = ScrollingMovementMethod()
-        send_button!!.setOnClickListener(this)
+
+        grpc_response_text.movementMethod = ScrollingMovementMethod()
+
+        send_button.setOnClickListener {
+            (getSystemService(Context.INPUT_METHOD_SERVICE) as InputMethodManager)
+                .hideSoftInputFromWindow(host_edit_text.windowToken, 0)
+            send_button.isEnabled = false
+            grpc_response_text.text = ""
+
+            GlobalScope.launch {
+                val result = sendMessage(
+                    host_edit_text.text.toString(),
+                    message_edit_text.text.toString(),
+                    port_edit_text.text.toString()
+                )
+                withContext(Dispatchers.Main) {
+                    onMessageReceived(result)
+                }
+            }
+        }
     }
 
-    override fun onClick(view: View) {
-        (getSystemService(Context.INPUT_METHOD_SERVICE) as InputMethodManager)
-            .hideSoftInputFromWindow(host_edit_text!!.windowToken, 0)
-        send_button!!.isEnabled = false
-        grpc_response_text!!.text = ""
-        GrpcTask(this)
-            .execute(
-                host_edit_text!!.text.toString(),
-                message_edit_text!!.text.toString(),
-                port_edit_text!!.text.toString()
-            )
+    private fun onMessageReceived(result: String) {
+        try {
+            channel?.shutdown()?.awaitTermination(1, TimeUnit.SECONDS)
+        } catch (e: InterruptedException) {
+            e.printStackTrace()
+        }
+
+        grpc_response_text.text = result
+        send_button.isEnabled = true
     }
 
-    private class GrpcTask constructor(activity: Activity) : AsyncTask<String, Void, String>() {
-        private val activityReference: WeakReference<Activity> = WeakReference(activity)
-        private var channel: ManagedChannel? = null
-
-        override fun doInBackground(vararg params: String): String {
-            val host = params[0]
-            val message = params[1]
-            val portStr = params[2]
-            val port = if (TextUtils.isEmpty(portStr)) 0 else Integer.valueOf(portStr)
-            return try {
-                channel = ManagedChannelBuilder.forAddress(host, port).usePlaintext().build()
+    @WorkerThread
+    suspend fun sendMessage(host: String, message: String, portStr: String): String {
+        return withContext(Dispatchers.IO) {
+            try {
+                val port = if (portStr.isEmpty()) 0 else portStr.toInt()
+                val channel = ManagedChannelBuilder.forAddress(host, port).usePlaintext().build()
                 val stub = GreeterGrpc.newBlockingStub(channel)
                 val request = HelloRequest.newBuilder().setName(message).build()
                 val reply = stub.sayHello(request)
@@ -60,24 +72,8 @@ class MainActivity : AppCompatActivity(), View.OnClickListener {
                 val pw = PrintWriter(sw)
                 e.printStackTrace(pw)
                 pw.flush()
-
-                "Failed... : %s".format(sw)
+                "Call failed: $sw"
             }
-        }
-
-        override fun onPostExecute(result: String) {
-            try {
-                channel?.shutdown()?.awaitTermination(1, TimeUnit.SECONDS)
-            } catch (e: InterruptedException) {
-                Thread.currentThread().interrupt()
-            }
-
-            val activity = activityReference.get() ?: return
-            val resultText: TextView = activity.findViewById(R.id.grpc_response_text)
-            val sendButton: Button = activity.findViewById(R.id.send_button)
-
-            resultText.text = result
-            sendButton.isEnabled = true
         }
     }
 }
